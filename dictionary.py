a7e49b49ca6fecd969519685cf3a128f1350da1d
saiyvainnee <sayidinaramadan12@gmail.com>
2024-11-24 19:00:33 +0700

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,110 @@
+
+data_mahasiswa = {}
+
+def lihat_data():
+    if not data_mahasiswa:
+        print("Daftar Nilai")
+        print("=" * 81)
+        print(f"{'No'.center(5)}|{'Nama'.center(15)}|{'NIM'.center(10)}|{'Nilai Tugas'.center(13)}|{'Nilai UTS'.center(10)}|{'Nilai UAS'.center(10)}|{'Nilai Akhir'.center(10)}|")
+        print("=" * 81)
+        print(f"{'TIDAK ADA DATA'.center(75)}")
+        print("=" * 81)
+        return
+    print("Daftar Nilai")
+    print("=" * 81)
+    print(f"{'No'.center(5)}|{'Nama'.center(15)}|{'NIM'.center(10)}|{'Nilai Tugas'.center(13)}|{'Nilai UTS'.center(10)}|{'Nilai UAS'.center(10)}|{'Nilai Akhir'.center(10)}|")
+    print("=" * 81)
+    for i, (nim, mhs) in enumerate(data_mahasiswa.items(), start=1):
+        print(f"{str(i).center(5)}|{mhs['nama'].ljust(15)}|{nim.center(10)}|{str(mhs['tugas']).center(13)}|{str(mhs['uts']).center(10)}|{str(mhs['uas']).center(10)}|{format(mhs['nilai_akhir'], '.2f').center(10)} |")
+    print("=" * 81)
+
+def tambah_data():
+    print("Tambah Data")
+    nim = input("NIM: ")
+    if nim in data_mahasiswa:
+        print("Data dengan NIM tersebut sudah ada!")
+        return
+    nama = input("Nama: ")
+    tugas = float(input("Nilai Tugas: "))
+    uts = float(input("Nilai UTS: "))
+    uas = float(input("Nilai UAS: "))
+    nilai_akhir = (tugas * 0.3) + (uts * 0.35) + (uas * 0.35)
+
+    data_mahasiswa[nim] = {
+        "nama": nama,
+        "tugas": tugas,
+        "uts": uts,
+        "uas": uas,
+        "nilai_akhir": nilai_akhir
+    }
+    print("Data berhasil ditambahkan!")
+
+def ubah_data():
+    lihat_data()
+    if not data_mahasiswa:
+        return
+    
+    nim = input("Masukkan NIM data yang akan diubah: ")
+    if nim in data_mahasiswa:
+        print("Masukkan Data Baru")
+        nama = input("Nama: ")
+        tugas = float(input("Nilai Tugas: "))
+        uts = float(input("Nilai UTS: "))
+        uas = float(input("Nilai UAS: "))
+        nilai_akhir = (tugas * 0.3) + (uts * 0.35) + (uas * 0.35)
+
+        data_mahasiswa[nim] = {
+            "nama": nama,
+            "tugas": tugas,
+            "uts": uts,
+            "uas": uas,
+            "nilai_akhir": nilai_akhir
+        }
+        print("Data berhasil diubah!")
+    else:
+        print("NIM tidak ditemukan!")
+
+def hapus_data():
+    lihat_data()
+    if not data_mahasiswa:
+        return
+
+    nim = input("Masukkan NIM data yang akan dihapus: ")
+    if nim in data_mahasiswa:
+        del data_mahasiswa[nim]
+        print("Data berhasil dihapus!")
+    else:
+        print("NIM tidak ditemukan!")
+
+def cari_data():
+    keyword = input("Masukkan nama atau NIM yang ingin dicari: ")
+    hasil_cari = {nim: mhs for nim, mhs in data_mahasiswa.items() if keyword.lower() in mhs['nama'].lower() or keyword in nim}
+    
+    if hasil_cari:
+        print("Hasil Pencarian:")
+        print("=" * 81)
+        print(f"{'No'.center(5)}|{'Nama'.center(15)}|{'NIM'.center(10)}|{'Nilai Tugas'.center(13)}|{'Nilai UTS'.center(10)}|{'Nilai UAS'.center(10)}|{'Nilai Akhir'.center(10)}|")
+        print("=" * 81)
+        for i, (nim, mhs) in enumerate(hasil_cari.items(), start=1):
+            print(f"{str(i).center(5)}|{mhs['nama'].ljust(15)}|{nim.center(10)}|{str(mhs['tugas']).center(13)}|{str(mhs['uts']).center(10)}|{str(mhs['uas']).center(10)}|{format(mhs['nilai_akhir'], '.2f').center(10)}|")
+        print("=" * 81)
+    else:
+        print("Data tidak ditemukan.")
+
+while True:
+    pilihan = input("[(L)ihat (T)ambah (U)bah (H)apus (C)ari (K)eluar] : ").lower()
+    if pilihan == 't':
+        tambah_data()
+    elif pilihan == 'u':
+        ubah_data()
+    elif pilihan == 'h':
+        hapus_data()
+    elif pilihan == 'l':
+        lihat_data()
+    elif pilihan == 'c':
+        cari_data()
+    elif pilihan == 'k':
+        print("Program selesai.")
+        break
+    else:
+        print("Pilihan tidak valid!")
